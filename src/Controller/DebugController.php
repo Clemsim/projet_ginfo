fcb9d9a817bcbea59167f13032c98d41f863bd39
Clément Jallat <clement.jallat@gmail.com>
2024-12-18 16:55:23 +0100

    Creation of a debug controller to verify db, symfony doesnt work

---
+++ src/Controller/DebugController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class DebugController extends AbstractController
+{
+    #[Route('/debug', name: 'app_debug')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $forma_repo = $entityManager->getRepository(Formation::class);
+        $formations = $forma_repo->findAll();
+        $formateur_repo = $entityManager->getRepository(Formateur::class);
+        $formateurs = $formateur_repo->findAll();
+        return $this->render('debug/index.html.twig', [
+            'controller_name' => 'DebugController',
+            'formations' => $formations,
+            'formateurs' => $formateurs,
+        ]);
+    }
+}
